46677369ce6566ab5175261408d3bf12305f0121
Lara Willson <LaraWillson@Laras-MacBook-Pro.local>
2016-02-04 02:09:22 -0500

    Week 4 - Tic Tac Toe - Attempt 1
    Completed this week’s assignment

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,4 +1,4 @@
-def printBoard(board):
+
     # TO DO #################################################################
     # Write code in this function that prints the game board.               #
     # The code in this function should only print, the user should NOT      #
@@ -6,9 +6,17 @@ def printBoard(board):
     #                                                                       #
     # Hint: you can follow the same process that was done in the textbook.  #
     #########################################################################
+    
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
-def checkWinner(board, player):    
-    print('Checking if ' + player + ' is a winner...')
+theBoard={'top-L':'','top-M':'','top-R':'',
+          'mid-L':'','mid-M':'','mid-R':'',
+          'low-L':'','low-M':'','low-R':''}
     
     # TO DO #################################################################
     # Write code in this function that checks the tic-tac-toe board          #
@@ -18,31 +26,41 @@ def checkWinner(board, player):
     # variable 'player' has won. The function should return False           #
     # if the player in the variable 'player' has not won.                   #
     #########################################################################
-    
-    
-def startGame(startingPlayer, board):
+
+def checkWinner(board, player):    
+    print('Checking if ' + player + ' is a winner...')
+    return((board['top-L']==player and board['top-M']==player and board['top-R']) or #top horizontal
+           (board['mid-L']==player and board['mid-M']==player and board['mid-R']) or #middle horizontal
+           (board['low-L']==player and board['low-M']==player and board['low-R']) or #bottom horizontal
+           (board['top-L']==player and board['mid-L']==player and board['low-L']) or #left vertical
+           (board['top-M']==player and board['mid-M']==player and board['low-M']) or #middle vertical
+           (board['top-R']==player and board['mid-R']==player and board['low-R']) or #right vertical
+           (board['top-L']==player and board['mid-M']==player and board['low-R']) or #diagonal
+           (board['top-R']==player and board['mid-M']==player and board['low-L']))  #diagonal
+
     # TO DO #################################################################
     # Add comments to each line in this function to describe what           #
     # is happening. You do not need to modify any of the Python code        #
     #########################################################################
 
-    turn = startingPlayer
-    for i in range(9):
-        printBoard(board)
-        print('Turn for ' + turn + '. Move on which space?')
-        move = input()
-        board[move] = turn
-        if( checkWinner(board, 'X') ):
-            print('X wins!')
-            break
-        elif ( checkWinner(board, 'O') ):
-            print('O wins!')
-            break
+def startGame(startingPlayer, board): 
+    turn = startingPlayer #Here we designate the starting play/start of the game.
+    for i in range(9): #Declares a for loop for the range of the 9 move options.
+        printBoard(board) #Here the program prints the tic tac toe board.
+        print('Turn for ' + turn + '. Move on which space?') #Here the program is asking the user whose turn it is what space they want to move.
+        move = input() #Here is the user's decision as to where to move.
+        board[move] = turn #Here we illustrate the board with the player's move on it.
+        if( checkWinner(board, 'X') ): #Here the program calls the checkWinner function above to verify whether the X player is a winner.
+            print('X wins!') #Here the program informs the X player they have won.
+            break #Here the program breaks out of the for loop since a winner has been designated.
+        elif ( checkWinner(board, 'O') ): #Here the program calls the checkWinner function above to verify whether the O player is a winner.
+            print('O wins!') #Here the program informs the O player they have won.
+            break #Here the program breaks out of the for loop since a winner has been designated.
     
-        if turn == 'X':
+        if turn == 'X':  #Here we are saying if it isn't X's turn it is O's turn.
             turn = 'O'
         else:
-            turn = 'X'
-        
+            turn = 'X' #Here we are saying if it isn't O's turn it is X's turn.
+
     printBoard(board)
     
